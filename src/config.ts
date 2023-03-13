cfdc5c77a9a45113fd5d2b04cf120ac3916055f5
JoeHooijmaijers <31613736+JoeHooijmaijers@users.noreply.github.com>
2023-03-13 19:17:47 +0100

    Physics base
    Edited config.js to fix wrong syntax
    
    Added basic matter physics objects into scene.

--- src/config.ts
+++ src/config.ts
@@ -2,18 +2,21 @@ import Phaser from 'phaser';
 
 export default{
     type: Phaser.AUTO,
+    width: 275,
+    height: 812,
     parent : 'mainGame',
     scale : {
         width: 375,
         height: 812,
         mode: Phaser.Scale.FIT,
-        physics: {
-            default: 'matter',
-            matter:{
-                gravity:{
-                    y:300
-                },
-                debug : true
+    },
+    physics: {
+        default: 'matter',
+        matter:{
+            debug : true,
+            gravity: {
+                x:0,
+                y:.1,
             }
         }
     }
--- src/scenes/gamescene.ts
+++ src/scenes/gamescene.ts
@@ -2,15 +2,29 @@ import Phaser from 'phaser';
 
 export default class GameScene extends Phaser.Scene{
     constructor(){
-        super('gamescene');
+        super('GameScene');
     }
 
     preload(){
-        
+        //Stacksprites
+        this.load.atlas('sheet','assets/sprite_stacking@1x' ,'assets/sprite_stacking_atlas@1x.json');
+        this.load.json('shapes', 'assets/physics_json@1x.json');
+
+        //(Back)ground
+        this.load.image('ground', 'assets/ground@1x.png');
+        this.load.json('groundshape', 'assets/ground_physics_json@1x.json')
+        this.load.image('background', 'assets/background@1x.png');
     }
 
     create(){
+        const { width, height} = this.cameras.main;
+        let shapes = this.cache.json.get('shapes');
         
+        this.add.image(187, 406, 'background');
+        this.matter.add.rectangle(100, 100, 100, 100);
+        this.matter.add.image(width/2, height-40, 'ground');
+        console.log(this.matter.world)
+        const bounds = this.matter.world.setBounds(0,0,width, height);
     }
 
     update(time: number, delta: number): void {
