29139fc36a5a3377f412ae09b6b3d40d5d5872a3
JoeHooijmaijers <31613736+JoeHooijmaijers@users.noreply.github.com>
2023-03-14 16:30:28 +0100

    Spawner Randomization
    Spawner now changes the next object spawned.
    Spawner now changes its own sprite based on the object to be spawned.
    Spawner now changes the collider of a spawned item based on the object to be spawned

--- src/classes/objectSpawner.ts
+++ src/classes/objectSpawner.ts
@@ -5,6 +5,8 @@ export default class ObjectSpawner extends Phaser.GameObjects.Sprite{
 
     spawnrange : [-50, 50];
     shapes;
+    currentShape;
+    
     constructor(scene: Phaser.Scene ,x: number, y:number, texture: string, frame: string){
         super(scene, x, y, texture, frame);
         this.DoTween();
@@ -27,6 +29,34 @@ export default class ObjectSpawner extends Phaser.GameObjects.Sprite{
         this.shapes = this.scene.cache.json.get('shapes');
 
         var sprite = this.scene.add.sprite(this.x,this.y, this.texture, this.frame.name);
-        var obj = this.scene.matter.add.gameObject(sprite,{shape: this.shapes.stack1},true);
+        var obj = this.scene.matter.add.gameObject(sprite,{shape: this.currentShape},true);
+        this.GetNextShape();
+    }
+
+    GetNextShape(){
+        let rnd :string = 'stack-' + Phaser.Math.Between(1, 5);
+        this.setTexture('sprites', rnd);
+    }
+
+    GetShapesByIndex(index : number){
+        switch (index) {
+            case 1:
+                this.currentShape = this.shapes.stack1;
+                break;
+            case 2:
+                this.currentShape = this.shapes.stack2;
+                break;
+            case 3:
+                this.currentShape = this.shapes.stack3;
+                break;
+            case 4:
+                this.currentShape = this.shapes.stack4;
+                break;
+            case 5:
+                this.currentShape = this.shapes.stack5;
+                break;
+            default:
+                break;
+        }
     }
 }
