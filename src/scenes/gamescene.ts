a2e76ed5d8f070df034ae54da4e3f1775bf4d599
JoeHooijmaijers <31613736+JoeHooijmaijers@users.noreply.github.com>
2023-03-14 14:07:58 +0100

    groundplacement

--- src/scenes/gamescene.ts
+++ src/scenes/gamescene.ts
@@ -21,16 +21,21 @@ export default class GameScene extends Phaser.Scene{
     create(){
         const { width, height} = this.cameras.main;
         var shapes = this.cache.json.get('shapes');
-        var shapesJSON = this.game.cache.json.get('shapes');
+        var groundShape = this.cache.json.get('groundshape');
         console.log(shapes);
-        console.log(shapesJSON.stack1.fixtures[0].vertices);
-        var bodyPhysics : Phaser.Types.Physics.Matter.MatterBodyConfig = shapesJSON;
+
         let spawner = new ObjectSpawner(this, width -20, 100, 'sprites', 'stack-1');
+
         this.add.image(187, 406, 'background');
+
         let deez = this.add.sprite(100, 100, 'sprites', 'stack-1');
         let nuts = this.matter.add.gameObject(deez, {shape: shapes.stack1},true);
+
+        let ground = this.matter.add.gameObject(this.add.sprite(width/2, height-40, 'ground'), {shape: groundShape.ground}, true);
+
         console.log(nuts)
-        this.matter.add.image(width/2, height-40, 'ground');
+        
+        //this.matter.add.image(width/2, height-40, 'ground');
         const bounds = this.matter.world.setBounds(0,0,width, height);
         this.add.existing(spawner);
     }
